b0e34e026d1ab4115a8af8c871f954d460bff5d8
Erick Fabricio Martínez Castellanos <mail@erickfabricio.com>
2019-10-29 14:51:16 -0600

    Implementación de servicio Login

--- src/app/main/components/login/login.component.ts
+++ src/app/main/components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EntityService } from 'src/app/entity/services/entity.service';
+import { UserModel } from 'src/app/entity/models/user.model';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'mail-login',
@@ -7,9 +12,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  //Form  
+  form: FormGroup;
+  user: UserModel;
+  hide: boolean = true;
+
+  constructor(private entityService: EntityService, private sessionService: SessionService, private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = new FormGroup({
+      mail: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  //************ ACTIONS OF FORM ************//
+
+  login() {
+    if (this.form.valid) {
+      //Assignment of values
+      this.user = new UserModel();
+      this.user.mail = String(this.form.get('mail').value).trim();
+      this.user.password = String(this.form.get('password').value).trim();
+
+      //Api      
+      this.sessionService.login(this.user).subscribe(resp => { 
+        console.log(resp);
+
+        if(resp.ok){        
+          let succesMessage = resp.menssage + " token: " + resp.token;        
+          this.openSnackBar(succesMessage, "X", "snackbar-success");
+        }else{
+          let succesMessage = resp.menssage;
+          this.openSnackBar(succesMessage, "X", "snackbar-success");
+        }
+
+      });
+
+      
+
+      
+
+
+      
+
+
+      //Succes
+      //let succesMessage = "Login user: " + this.user.mail;
+      //this.openSnackBar(succesMessage, "X", "snackbar-success");
+      //this.createForm();
+    } else {
+      //Error
+      let errorMessage = "¡Invalid form, " + this.validateForm() + "!";
+      this.openSnackBar(errorMessage, "X", "snackbar-danger");
+    }
+  }
+
+  //************ FORM VIDATION ************//
+
+  validateForm() {
+
+    if (this.form.get('mail').invalid) {
+      return this.getErrorMessageMail();
+    }
+
+    if (this.form.get('password').invalid) {
+      return this.getErrorMessagePassword();
+    }
+
+  }
+
+  getErrorMessageMail() {
+    if (this.form.get('mail').hasError('required')) {
+      return 'Mail is required';
+    }
+    if (this.form.get('mail').hasError('email')) {
+      return 'Invalid email';
+    }
+  }
+
+  getErrorMessagePassword() {
+    if (this.form.get('password').hasError('required')) {
+      return 'Password is required';
+    }
+  }
+
+  openSnackBar(message: string, action: string, style: string) {
+    this._snackBar.open(
+      message,
+      action,
+      {
+        duration: 2000,
+        verticalPosition: 'top',
+        panelClass: [style]
+      }
+    );
   }
 
 }
